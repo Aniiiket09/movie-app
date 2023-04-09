b9d59eec1e49d9fd39d5202d03090cb69e8e6a17
Aniket <aniket.v.tarapurkar99@gmail.com>
2023-04-09 16:34:41 +0530

    installing redux from node added store and see the intial state is what thats it

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { legacy_createStore } from "redux";
+
 import "./index.css";
 import App from "./components/App";
-
+import movies from "./reducer";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const store = legacy_createStore(movies);
+console.log("store ", store);
+console.log("State ", store.getState());
 root.render(
   <React.StrictMode>
     <App />
