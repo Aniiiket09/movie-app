fb16b2cef0e89b8086fb30232779c4c8277a77be
Aniket <aniket.v.tarapurkar99@gmail.com>
2023-04-11 12:24:48 +0530

    introdunction to middle ware and importance of next() word in middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { legacy_createStore } from "redux";
+import { legacy_createStore, applyMiddleware } from "redux";
 
 import "./index.css";
 import App from "./components/App";
 import rootReducer from "./reducer";
+
+//fuction logger(obj,dispatch, getState)
+//logger(obj)(dispatch)(getState)
+const logger = function ({ dispatch, getState }) {
+  return function (next) {
+    return function (action) {
+      //middleware code
+      console.log("ACTION_TYPE=", action.type);
+      next(action);
+    };
+  };
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const store = legacy_createStore(rootReducer);
+const store = legacy_createStore(rootReducer, applyMiddleware(logger));
 console.log("store ", store);
 // console.log(" Before State ", store.getState());
 
